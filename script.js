fe44979cc24b17b8b5a8b781a67d5d88a3dea835
Bishwasa <giribiswas415@gmail.com>
2024-09-27 14:59:25 +0530

    REQUIRED TWO

--- script.js
+++ script.js
@@ -8,5 +8,33 @@ $(function(){
 
 
 $(document).ready(function() {
-    $(".skitter-large").skitter();
+    $('.skitter-large').skitter({
+        navigation: true,
+        dots: false,
+        theme:"clean"
+      });
+  });
+
+
+  $(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:60,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+                items:3,
+                nav:false
+            },
+            1000:{
+                items:5,
+                nav:false,
+                loop:true
+            }
+        }
+    })
   });
